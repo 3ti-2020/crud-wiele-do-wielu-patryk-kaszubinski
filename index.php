8d3a85091c3f5a22f1d23c8fd65a824de1e4c895
PatrykK01 <42960742+PatrykK01@users.noreply.github.com>
2020-10-27 14:05:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -8,22 +8,33 @@
 </head>
 <body>
     <?php
-        $servername="127.0.0.1";
+        $servername="localhost";
         $username="root";
         $password="";
-        $dbname="biblioteka";
+        $dbname="biblioteka2";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
-        $result = $conn->query();
+        $result = $conn->query("SELECT * FROM wypozyczenia");
 
-        echo("<table>");
+        echo("<table style='border: 1px solid black'>");
+        echo("<tr>
+        <td style='border: 1px solid black'>id_wyp</td>
+        <td style='border: 1px solid black'>id_a</td>
+        <td style='border: 1px solid black'>id_ks</td>
+        </tr>");
+
         while($wiersz = $result->fetch_assoc()){
-            echo("tr");
-            echo("<td>".$wiersz['']."</td>");
-            echo("/tr");
+            echo("<tr>");
+            echo("<td style='border: 1px solid black'>".$wiersz['id_wyp']."<td>".$wiersz['id_a']."<td>".$wiersz['id_ks']);
+            echo("</tr>");
         }
         echo("</table>");
     ?>
+    <form action="insert.php" method="POST">
+    id_a<input type="text" name="id_a">
+    id_ks<input type="text" name="id_ks">
+    <input type="submit" value="Wyslij" method="POST">
+    </form>
 </body>
 </html>
