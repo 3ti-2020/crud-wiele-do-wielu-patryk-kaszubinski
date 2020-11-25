11bdfef4f94e5c69bc0d2ca1b021aa2b8b22e8a4
PatrykK01 <42960742+PatrykK01@users.noreply.github.com>
2020-11-25 10:56:10 +0100

    Add files via upload

---
+++ todo/script.js
@@ -0,0 +1,32 @@
+const todoList = document.querySelector("#todoList");
+const todoForm = document.querySelector("#todoForm");
+const todoSearch = document.querySelector("#todoSearch");
+const todoTextarea = todoForm.querySelector('textarea');
+
+function addTask(text) 
+{
+    console.log("Dodaję zadanie do listy")
+}
+
+todoForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if (todoTextarea.value !== "") 
+    {
+        addTask(todoTextarea.value);
+        todoTextarea.value = "";
+    }
+});
+
+function addTask(text) {
+    const element = document.createElement("div");
+    element.classList.add("element");
+
+    const elementInner = document.querySelector("#elementTemplate").content.cloneNode(true);
+
+    element.append(elementInner);
+
+    element.querySelector(".element-text").innerText = text;
+
+    todoList.append(element);
+};
