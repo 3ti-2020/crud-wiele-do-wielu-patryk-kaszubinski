87254a2ccedbc4ad57f6d7f1d2fb9b29b3b71c1c
PatrykK01 <42960742+PatrykK01@users.noreply.github.com>
2020-12-16 10:55:10 +0100

    Add files via upload

--- blog/index.php
+++ blog/index.php
@@ -11,7 +11,7 @@
     <div class="top">
         <h1><i>BLOG</i></h1>
         <?php 
-        if(isset($_GET['szukaj_tag'])){
+        if(isset($_GET['znajdz'])){
         echo("<a href='index.php'>RESET</a>");
         }
         ?>
@@ -31,14 +31,14 @@
 
 
 
-    if(isset($_GET['szukaj_tag'])){
-
-        $result = $conn->query("SELECT * FROM post, post_tag, tag WHERE post_tag.post_id=post.id AND tag.tag = '".$_GET['szukaj_tag']."' AND tag.id = post_tag.tag_id");
+    if(isset($_GET['znajdz'])){
+        $result = $conn->query("SELECT Distinct post, zaw, post.id, img FROM post, post_tag, tag WHERE post_tag.post_id=post.id AND tag.id = post_tag.tag_id AND tag.tag = '".$_GET['znajdz']."'");
     }else{
-        $result = $conn->query("SELECT * FROM post");
+        $result = $conn->query("SELECT DISTINCT * FROM post");
     }
 
     while($wiersz = $result->fetch_assoc()){
+
         echo("<div class='card'>");
         echo("<div class='header'>");
         echo("<h1>".$wiersz['post']."</h1>");
@@ -46,10 +46,8 @@
 
             echo("<div class='formu'>");
             while($wiersz3 = $result3->fetch_assoc()){
-        
-
                 echo("<form action='index.php' method='GET'>
-                <input type='submit' name='szukaj_tag' value='".$wiersz3['tag']."'>
+                <input type='submit' name='znajdz' value='".$wiersz3['tag']."'>
                 </form>");
             }
             echo("</div>");
